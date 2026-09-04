0fc77755d798b8ff575d2598ceb98f41e68843fe
bmtbmt23 <bbeck1@byupathway.edu>
2026-09-04 17:36:48 -0600

    week1

--- scripts/script.js
+++ scripts/script.js
@@ -12,3 +12,7 @@ navButtom.addEventListener('click', () => {
   navButtom.classList.toggle('show');
   navBar.classList.toggle('show');
 })
+
+let d = new Date();
+document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()} Bruna Beck`;
+document.getElementById("lastModified").textContent = `lastModified:${document.lastModified}`;
