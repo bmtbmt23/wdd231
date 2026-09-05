ca859d87c8b39162297874e603b26ab048226790
bmtbmt23 <bbeck1@byupathway.edu>
2026-09-04 19:10:57 -0600

    week1

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,3 @@
-const navButton = document.querySelector('#ham-btn');
-const nav = document.querySelector('nav');
-
-navButton.addEventListener('click', () =>{
-  navButtom.classList.toggle('show');
-  nav.classList.toggle('show');
-});
-
-const navBar = document.querySelector('#nav-home');
-
-navButtom.addEventListener('click', () => {
-  navButtom.classList.toggle('show');
-  navBar.classList.toggle('show');
-})
-
 let d = new Date();
 document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()} Bruna Beck`;
 document.getElementById("lastModified").textContent = `lastModified:${document.lastModified}`;
---
+++ week1/scripts/script.js
@@ -0,0 +1,18 @@
+let d = new Date();
+document.getElementById("currentYear").innerHTML = `&copy;${d.getFullYear()} Bruna Beck`;
+document.getElementById("lastModified").textContent = `lastModified:${document.lastModified}`;
+
+const navButton = document.querySelector('#ham-btn');
+const nav = document.querySelector('nav');
+
+navButton.addEventListener('click', () =>{
+  navButtom.classList.toggle('show');
+  nav.classList.toggle('show');
+});
+
+const navBar = document.querySelector('#nav-home');
+
+navButtom.addEventListener('click', () => {
+  navButtom.classList.toggle('show');
+  navBar.classList.toggle('show');
+})
