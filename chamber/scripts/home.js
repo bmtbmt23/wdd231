3f6fbcf96dd909978c7b6c644dfc725e50898839
bmtbmt23 <bbeck1@byupathway.edu>
2026-09-19 19:32:02 -0600

    week3

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -4,3 +4,8 @@ document.getElementById("lastModified").textContent = `lastModified: ${document.
 
 const hamButton = document.querySelector("#ham-btn");
 const navigation = document.querySelector("#navigation");
+
+hamButton.addEventListener("click", () =>{
+    navigation.classList.toggle("show");
+    hamButton.classList.toggle("show");
+})
