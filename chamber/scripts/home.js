2afd9fd40e38768d94a8d66def2c03fd3c37e55a
bmtbmt23 <bbeck1@byupathway.edu>
2026-09-19 21:50:25 -0600

    week3

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -9,3 +9,32 @@ hamButton.addEventListener("click", () =>{
     navigation.classList.toggle("show");
     hamButton.classList.toggle("show");
 })
+
+const threeMembers = "data/members.json";
+const spotlightContainer = document.querySelector("#spotlight-container");
+async function getMembersSpotlight(){
+    const response = await fetch(threeMembers);
+    const data = await response.json();
+    const members = data.members.filter(
+        member => member.membership === 2 || member.membership === 3
+    );
+
+    members.sort(() => Math.random() -0.5);
+    const chooseMembers = members.slice(0, 3);
+    
+    chooseMembers.forEach(member =>{
+        const card = document.createElement("article");
+        card.classList.add("spotlight-card");
+
+        card.innerHTML = ` 
+        <h3>${member.name}</h3>
+        <img src="${member.image}" alt="${member.name}">
+        <p>${member.phone}</p>
+        <p>${member.membership === 3 ? "Gold" : "Silver"}</p>
+        <a href="${member.website}" target="_blank">Website</a>
+        `;
+
+        spotlightContainer.appendChild(card);
+    });
+}
+getMembersSpotlight();
